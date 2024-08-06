198ac73e86e790e155e53d9e96787ae69cd4aabb
Your Name <domtuz@yahoo.com>
2024-08-06 06:35:31 -0400

    built application, routes and handler functions

---
+++ main.go
@@ -0,0 +1,19 @@
+package main 
+
+import (
+	"context"
+	"fmt"
+
+	// "github.com/DomTuz/Microservice-API/application"
+	"github.com/DomTuz/Capstone-Mini-Project-Microservice-API/application"
+
+)
+
+func main() {
+	app := application.New()
+
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("Failed to start app:", err)
+	}
+} 
