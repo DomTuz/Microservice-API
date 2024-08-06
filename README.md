43d518a61a1acf55f91ce143ae9afeb87718f277
Your Name <domtuz@yahoo.com>
2024-08-06 11:06:28 -0400

    updated module in routes.go

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Microservice-API
+
+Hello everyone! In this repo, I have decided to walk through a tutorial created by Net Ninja in preperation for my upcoming project.
--- application/routes.go
+++ application/routes.go
@@ -6,7 +6,7 @@ import(
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
-	"github.com/DomTuz/Microservice-API/handler"
+	"github.com/DomTuz/Microservice-API/application/handler"
 )
 
 func loadRoutes() *chi.Mux {
